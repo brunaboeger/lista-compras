099f6c17d050663fdf05962d43997fd08da4bf8b
Bruna <53488109+brunaboeger@users.noreply.github.com>
2025-08-12 11:02:17 -0300

    feat: added card component from shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle
+} from "@/components/ui/card";
+
 export default function Home() {
   return (
     <div>
-      <section className="flex p-6">
-        <h1>Home</h1>
+      <section className="flex flex-col gap-4 p-5 bg-gray-200">
+        <h1 className="text-3xl font-extrabold">Seu carrinho</h1>
+        <div className="flex gap-2 flex-wrap">
+          <Card>
+            <CardContent>
+              <CardTitle>Título</CardTitle>
+              <CardDescription>Descrição</CardDescription>
+            </CardContent>
+          </Card>
+        </div>
       </section>
     </div>
   );

a57dc74888af03036d6657f240ca0dea962a8145
Bruna <53488109+brunaboeger@users.noreply.github.com>
2025-08-12 11:02:47 -0300

    feat: created the header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="flex items-center gap-2 p-5 bg-gray-100">
+      <div className="flex items-center justify-center border rounded-full w-[40px] h-[40px] bg-gray-950 text-white">
+        C
+      </div>
+      <h3 className="font-medium">Olá, Caitlyn</h3>
+    </header>
+  )
+}

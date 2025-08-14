d5c550b47e78743509db70fcb86a9a4678035ca6
Bruna <53488109+brunaboeger@users.noreply.github.com>
2025-08-14 15:22:54 -0300

    feat: added navigation between home and itens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="flex items-center gap-2 p-5 border-b shadow bg-white sticky top-0">
-      <div className="flex items-center justify-center border rounded-full w-[40px] h-[40px] bg-gray-950 text-white">
-        C
-      </div>
+      <Link href="/">
+        <div className="flex items-center justify-center border rounded-full w-[40px] h-[40px] bg-gray-950 text-white">
+          C
+        </div>
+      </Link>
       <h3 className="font-medium">Olá, Caitlyn</h3>
     </header>
   )
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input"
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 const Toolbar = () => {
   return (
     <footer className="flex gap-2 p-5 absolute w-full bottom-0 border-t bg-white shadow-2xl">
       <Input placeholder="Item..." />
-      <Button>Criar</Button>
+      <Button asChild>
+        <Link href="/itens">Criar</Link>
+      </Button>
     </footer>
   );
 }

278e3bd54973a0f53407bfd53c9ca323c29743f5
Bruna <53488109+brunaboeger@users.noreply.github.com>
2025-08-14 10:44:23 -0300

    feat: added examples to cart list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,103 @@
 import {
   Card,
-  CardContent,
   CardDescription,
   CardTitle
 } from "@/components/ui/card";
 
+import { SoapDispenserDropletIcon } from "lucide-react";
+
 export default function Home() {
   return (
     <div>
-      <section className="flex flex-col gap-4 p-5 bg-gray-200">
-        <h1 className="text-3xl font-extrabold">Seu carrinho</h1>
+      <section className="flex flex-col gap-4 p-5">
+        <h1 className="text-3xl font-extrabold mt-2">Seu carrinho</h1>
         <div className="flex gap-2 flex-wrap">
-          <Card>
-            <CardContent>
-              <CardTitle>Título</CardTitle>
-              <CardDescription>Descrição</CardDescription>
-            </CardContent>
+          {/* Vira componente */}
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+        </div>
+      </section>
+      <section className="flex flex-col gap-4 p-5">
+        <h2 className="text-3xl font-extrabold">Recentes</h2>
+        <div className="flex gap-2 flex-wrap">
+          {/* Vira componente */}
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
+          </Card>
+          <Card className="flex flex-col justify-center items-center p-4 gap-1 text-center min-w-[100px]">
+            <SoapDispenserDropletIcon />
+            <CardTitle className="mt-1">Sabonete</CardTitle>
+            <CardDescription>x1</CardDescription>
           </Card>
         </div>
       </section>

41a25d271e0459f45c6a6c6ad5050b546e4f70b6
Bruna <53488109+brunaboeger@users.noreply.github.com>
2025-08-14 15:23:20 -0300

    feat: added empty state to itens list

--- src/app/itens/page.tsx
+++ src/app/itens/page.tsx
@@ -3,6 +3,7 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState, useEffect } from "react";
+import { ShoppingBagIcon } from "lucide-react";
 
 const ItensPage = () => {
   const [item, setItem] = useState("");
@@ -10,8 +11,12 @@ const ItensPage = () => {
 
   // Carregar do localStorage ao abrir a página
   useEffect(() => {
-    const saved = localStorage.getItem("itens");
-    if (saved) setItens(JSON.parse(saved));
+    try {
+      const saved = localStorage.getItem("itens");
+      if (saved) setItens(JSON.parse(saved));
+    } catch (error) {
+      console.error("Erro ao ler o localStorage", error);
+    }
   }, []);
 
   // Salvar no localStorage sempre que itens mudar
@@ -36,18 +41,25 @@ const ItensPage = () => {
             onChange={(e) => setItem(e.target.value)}
             placeholder="Ex: Pão, Shampoo..."
           />
-          <Button onClick={addItem}>Adicionar</Button>
+          <Button className="cursor-pointer" onClick={addItem}>Adicionar</Button>
         </div>
       </div>
       <div className="p-5 mt-5 bg-white rounded-2xl border">
         <h2 className="text-2xl font-bold mb-5">Itens</h2>
-        <ul className="space-y-2">
-          {itens.map((i, index) => (
-            <li key={index} className="border p-2 rounded">
-              {i}
-            </li>
-          ))}
-        </ul>
+        {itens.length === 0 ? (
+          <div className="text-gray-500">
+            <ShoppingBagIcon className="mx-auto mb-2" />
+            <p className="text-center">Sem itens cadastrados</p>
+          </div>
+        ) : (
+          <ul className="space-y-2">
+            {itens.map((i, index) => (
+              <li key={index} className="border p-2 rounded">
+                {i}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );

a863fe6a2108fe790fde02a2cc69120fd9c07933
Bruna <53488109+brunaboeger@users.noreply.github.com>
2025-08-17 00:45:47 -0300

    style: changed the header background color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,15 @@ import { Button } from "@/components/ui/button";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between p-5 border-b shadow bg-white sticky top-0">
+    <header className="flex items-center justify-between p-5 border-b shadow bg-gray-950 text-white sticky top-0">
       <div className="flex items-center gap-2">
-        <div className="flex items-center justify-center border rounded-full w-[40px] h-[40px] bg-gray-950 text-white">
+        <div className="flex items-center justify-center border rounded-full w-[40px] h-[40px] bg-white text-gray-950">
           C
         </div>
         <h3 className="font-medium">Olá, Caitlyn</h3>
       </div>
       <div>
-        <Button variant="outline" asChild>
+        <Button variant="secondary" asChild>
           <Link href="/">Início</Link>
         </Button>
       </div>

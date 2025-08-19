4843354bef722d8c937c42b04c6657e4a849daf4
Bruna <53488109+brunaboeger@users.noreply.github.com>
2025-08-19 11:01:20 -0300

    feat: added first letter to user profile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 export default function Header() {
+  const username = "Olá!";
+
   return (
     <header className="p-5 border-b shadow bg-gray-950 text-white sticky top-0">
       <div className="flex items-center justify-between max-w-[1040px] mx-auto">
         <div className="flex items-center gap-2">
           <div className="flex items-center justify-center border rounded-full w-[40px] h-[40px] bg-white text-gray-950">
-            O
+            {username.charAt(0)}
           </div>
-          <h3 className="font-medium">Olá!</h3>
+          <h3 className="font-medium">{username}</h3>
         </div>
       </div>
     </header>
